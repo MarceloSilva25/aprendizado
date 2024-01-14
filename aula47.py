28f9f32ff1cbaf34cc18bdc5a1b8489b6583ff04
MarceloSilva25 <154945646+MarceloSilva25@users.noreply.github.com>
2024-01-14 19:51:57 -0300

    primeiro commit

---
+++ aula47.py
@@ -0,0 +1,19 @@
+nome = input("Digite seu nome: ")
+idade = input("digie sua idade: ")
+if nome and idade: 
+    print(f"Seu nome é: {nome}" )
+   
+    print(f"Seu nome invertido é: {nome [:: -1]}")
+   
+    print(f"seu nome contém {len (nome)} letras")
+
+    if  ' ' in nome:
+        print("Seu nome contém espaços")
+    else:
+        print("Seu nome não contém espaços")
+
+    print(f"A primeira letra do seu nome é: {nome [0]}")
+   
+    print(f"a ultima letra do seu nome é: {nome [-1]}")
+else: 
+    print("Desculpe, vc deixou campos em branco")
